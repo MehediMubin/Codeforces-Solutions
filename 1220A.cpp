c8cbcdfe9dcd386ea7869db5f8c52a5daaa5bc4d
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2020-04-06 20:28:48 +0600

    Add files via upload

---
+++ 1220A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fread freopen("in.txt", "r", stdin)
+#define fwrite freopen("out.txt", "w", stdout)
+
+int main ()
+{
+    int n;
+    scanf("%d", &n);
+    getchar();
+    char str[n + 5];
+
+    for (int i = 0; i < n; i++) scanf("%c", &str[i]);
+
+    if (n == 3) puts("1");
+
+    else if (n == 4) puts("0");
+
+    else {
+        int zero = 0;
+        for (int i = 0; i < n; i++) {
+            if (str[i] == 'n') printf("%d ", 1);
+            else if (str[i] == 'r') zero++;
+        }
+        for (int i = 0; i < zero; i++) printf("%d ", 0);
+    }
+
+    return 0;
+}
