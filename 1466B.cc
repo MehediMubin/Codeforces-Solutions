369b71dcdcc0ec08a921c48e97aa791f40146d9b
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-08-07 20:38:16 +0600

    Create 1466B.cc

---
+++ 1466B.cc
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define ll long long
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int main ()
+{
+    FastIO;
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        vector <int> v(n);
+        unordered_map <int, int> mp;
+        for (auto &i : v) {
+            cin >> i;
+            mp[i]++;
+        }
+
+        sort(all(v));
+        reverse(all(v));
+        for (int i = 0; i < v.size(); i++) {
+            if (!i) {
+                v[i]++;
+            }
+            else {
+                if (v[i] == v[i - 1]) continue;
+                if (v[i - 1] != (v[i] + 1)) v[i]++;
+            }
+        }
+        
+        set <int> s;
+        for (auto i : v) s.insert(i);
+        cout << s.size() << nl;
+    }
+
+    return 0;
+}
